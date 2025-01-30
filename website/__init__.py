88a25757aaf38467de8badf219f7d400d01698c0
Ívar Armín Derayat <ivararmind@Ivars-Air.c.is>
2025-01-30 18:58:00 +0000

    Revert to SQLite and remove PostgreSQL settings

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 from flask_migrate import Migrate
-import os
 
 
 
@@ -14,13 +13,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    DATABASE_URL = os.getenv("DATABASE_URL")
-
-    # Fix Heroku's old-style `postgres://` URL format
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL   
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     migrate.init_app(app, db)  
 
