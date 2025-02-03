498a5c953d3692deaae0673873d905a979ba823f
Ívar Armín Derayat <ivararmind@Ivars-MacBook-Air.local>
2025-02-03 15:15:31 +0000

    mhmh

--- website/__init__.py
+++ website/__init__.py
@@ -19,54 +19,54 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', os.getenv('DATABASE_URL'))
-    # Rest of your code...
-
+    
+    # Get the database URL from environment variables, fallback to a local SQLite database.
+    database_url = os.getenv('SQLALCHEMY_DATABASE_URI') or os.getenv('DATABASE_URL') or f"sqlite:///{DB_NAME}"
+    
+    # If using Heroku Postgres, the URL may begin with 'postgres://'
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    
+    # Initialize extensions
     db.init_app(app)
-    migrate.init_app(app, db)  
-
-
+    migrate.init_app(app, db)
+    
+    # Mail configuration
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USE_SSL'] = True
     app.config['MAIL_USERNAME'] = 'ivararmind@gmail.com'
     app.config['MAIL_PASSWORD'] = 'sbzc hfuy glqy xdvw'
-    mail.init_app(app)  # Initialize Mail with app here
-
-
-
+    mail.init_app(app)
+    
     @app.template_filter('format_thousands')
     def format_thousands(value):
         try:
             value = float(value)
             formatted_value = '{:,.2f}'.format(value).replace(",", "X").replace(".", ",").replace("X", ".")
-
             if formatted_value.endswith(",00"):
                 formatted_value = formatted_value[:-3] 
-
             return formatted_value
         except (ValueError, TypeError):
             return value
-
-
+    
     from .views import views
     from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
-    from .models import User, Log
     
+    from .models import User, Log
     with app.app_context():
         db.create_all()
-
+    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
+    
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
     
-
     return app
