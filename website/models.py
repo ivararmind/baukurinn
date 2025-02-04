49b374c8f73f9fa271aadc810f66ebc99fa47a6e
Ívar Armín Derayat <ivararmind@Ivars-Air.c.is>
2025-02-04 20:35:01 +0000

    Increase password column length to 300

--- website/models.py
+++ website/models.py
@@ -20,6 +20,6 @@ class Log(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(150), unique = True)
-    password = db.Column(db.String(150))
+    password = db.Column(db.String(300))
     first_name  = db.Column(db.String(150))
     logs = db.relationship('Log', backref='user', lazy=True)
